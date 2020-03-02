d3de4495f4e2adabd2acda9a518256303efdf431
Guita Vahdatinia <gvahdatinia@freewheel.tv>
2020-03-02 02:34:58 -0500

    Add in docker compose

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,11 @@
-import axios from 'axios';
-const API_ENDPOINT = 'http://localhost:8080/'
+import axios from "axios";
+const API_ENDPOINT =
+  process.env.NODE_ENV === "dev"
+    ? "http://localhost:8080/"
+    : "http://ec2-3-80-140-96.compute-1.amazonaws.com:8080/";
 
-const instance = axios.create({baseURL: API_ENDPOINT});
+const instance = axios.create({ baseURL: API_ENDPOINT });
 instance.CancelToken = axios.CancelToken;
 instance.isCancel = axios.isCancel;
-instance.withCredentials = true
-export default instance
+instance.withCredentials = true;
+export default instance;
